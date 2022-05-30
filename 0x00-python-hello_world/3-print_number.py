18ece563799cf65bb4cc81177fd0dc4e8c83170f
Odongoweri <99469707+Odongoweri@users.noreply.github.com>
2022-05-30 22:28:48 +0300

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
-#!/user/bin/python3[D[D[D[D[D[D[D[D[D[D[D[D[[3~[3~[3~[3~[[C[C[C[C[C[D[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[Cusr/bin/python3
+#!/user/bin/python3
 number = 98
 print("{:d} Battery street" .format(number))
