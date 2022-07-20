31400949ad6f39c7ad6aae5f5144d9940af62079
Odongoweri <margaretodongowerik@gmail.com>
2022-07-20 15:05:36 +0300

    python input_output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 5-to_json_string.py
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C"""Return the JSON representation of a string object."""
+    return json.dumps(my_obj)[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D                                                            [A[C[C[C[C"""Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
